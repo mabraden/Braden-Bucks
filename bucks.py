bee65c52daf13d05081319d7bc08b39f7f21c767
marcbraden <43180109+marcbraden@users.noreply.github.com>
2026-02-11 17:11:36 +0000

    Wed, Feb 11, 2026, 11:11 AM -06:00

--- bucks.py
+++ bucks.py
@@ -1,5 +1,11 @@
 import json
 def print_json(name):
     print(json.dumps(json.load(open(name,'r'))))
+def edit_json(data,filename):
+    file=load(filename,'r')
+    filedata=json.load(file)
+    filedata["transactions"].append(data)
+    file.seek(0)
+    json.dump(filedata,file)
+transaction={"from":"Braden","to":"Simonne","amount":15}
 
-transaction="from:Braden
