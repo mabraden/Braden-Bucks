4d75d74296f0d52b3b3359edf06bcab473fa9944
marcbraden <43180109+marcbraden@users.noreply.github.com>
2026-02-11 17:13:24 +0000

    Wed, Feb 11, 2026, 11:13 AM -06:00

--- bucks.py
+++ bucks.py
@@ -2,10 +2,12 @@ import json
 def print_json(name):
     print(json.dumps(json.load(open(name,'r'))))
 def edit_json(data,filename):
-    file=load(filename,'r')
+    file=open(filename,'r')
     filedata=json.load(file)
     filedata["transactions"].append(data)
     file.seek(0)
-    json.dump(filedata,file)
+    json.dump(filedata,file,indent=4)
+    file.truncate()
 transaction={"from":"Braden","to":"Simonne","amount":15}
+edit_json(transaction,"ledger.json")
 
